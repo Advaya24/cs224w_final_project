33f52f7f1cf4fb776be5f38b367e1453c09c3c1f
Mohamamd Ali Rehan <alirehan@DN51sk8q.SUNet>
2023-11-04 17:39:05 -0700

    Using DGL now

--- data/dataset.py
+++ data/dataset.py
@@ -4,23 +4,11 @@ from ogb.nodeproppred import PygNodePropPredDataset
 from torch_geometric.data import DataLoader, Dataset
 import torch
 from torch_geometric.data.data import BaseData
-
+import torch_geometric
+from copy import deepcopy
 # # Load the dataset
 # dataset = PygNodePropPredDataset(name = "ogbn-mag", root = 'dataset/')
 
- 
-# # Get split indices
-# split_idx = dataset.get_idx_split()
-
-# # dataset_dict = dict(dataset[0])
-# # print(dataset_dict)
-# # train_set = {prop: d['paper'][split_idx["train"]['paper']] for prop, d in dataset[0]}
-# num_nodes_dict = dataset[0]['num_nodes_dict']
-# edge_index_dict = dataset[0]['edge_index_dict']
-# x_dict = dataset[0]['x_dict']
-# node_year_dict = dataset[0]['node_year']
-# edge_reltype_dict = dataset[0]['edge_reltype']
-# y_dict = dataset[0]['y_dict']
 
 class CoAuthorPredDataset(Dataset):
     def __init__(self, name='ogbn-mag', root = 'dataset/', split = 'train'):
@@ -49,7 +37,41 @@ class CoAuthorPredDataset(Dataset):
         return self.x_dict, self.edge_index_dict, self.edge_reltype_dict, self.y_dict, self.node_year_dict
     
 
-dataset = CoAuthorPredDataset(name = "ogbn-mag", root = 'dataset/')
-print(dataset[0])
+# dataset = PygNodePropPredDataset(name = "ogbn-mag", root = 'dataset/')
+# graph=dataset[0]
+# graph.to('cpu')
+# import pdb;pdb.set_trace()
+# print(graph.edge_index.device)
+# train_idx = dataset.get_idx_split()["train"]
+# subgraph = torch_geometric.utils.subgraph(train_idx, graph)
+
+# print(dataset[0])
     
+
+from ogb.nodeproppred import DglNodePropPredDataset
+import dgl
+
+dataset = DglNodePropPredDataset(name = "ogbn-mag", root = 'dataset/')
+graph,labels=dataset[0]
+split_idx = dataset.get_idx_split()
+train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
+
+
+
+# g = dgl.heterograph({('user', 'plays', 'game'): ([0, 1, 1, 2], [0, 0, 2, 1]),('user', 'follows', 'user'): ([0, 1], [1, 3])})
+# sg, inverse_indices = dgl.khop_subgraph(g, {'game': 0}, k=2)
+
+
+
+train_graph= dgl.remove_nodes(graph, valid_idx['paper'],ntype='paper')
+
+k_hops=1
+paper_node_id=2
+# out_sampled_graph = dgl.sampling.sample_neighbors(train_graph, {'paper':[paper_node_id]},k_hops,edge_dir ='out'  )
+# in_sampled_graph = dgl.sampling.sample_neighbors(train_graph, {'paper':[paper_node_id]},k_hops,edge_dir ='in' )
+# merged_graph=dgl.merge([out_sampled_graph, in_sampled_graph])
+
+sampled_graph,inv_edges=dgl.khop_subgraph(train_graph,{'paper':[paper_node_id]},k_hops)
+
+
 
