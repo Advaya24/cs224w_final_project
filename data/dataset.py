29262bba59ad8b803f8985538606b3deb6cb0506
Mohamamd Ali Rehan <alirehan@DN51sk8q.SUNet>
2023-11-08 20:56:37 -0800

    author sampler added

--- data/dataset.py
+++ data/dataset.py
@@ -1,53 +1,3 @@
-# from typing import Union
-# import numpy as np
-# from ogb.nodeproppred import PygNodePropPredDataset
-# from torch_geometric.data import DataLoader, Dataset
-# import torch
-# from torch_geometric.data.data import BaseData
-# import torch_geometric
-# from copy import deepcopy
-# # Load the dataset
-# dataset = PygNodePropPredDataset(name = "ogbn-mag", root = 'dataset/')
-
-
-# class CoAuthorPredDataset(Dataset):
-#     def __init__(self, name='ogbn-mag', root = 'dataset/', split = 'train'):
-#         self.dataset = PygNodePropPredDataset(name = name, root = root)
-#         split_idx = self.dataset.get_idx_split()
-#         self.num_nodes_dict = self.dataset[0]['num_nodes_dict']
-#         self.edge_index_dict = self.dataset[0]['edge_index_dict']
-#         self.x_dict = self.dataset[0]['x_dict']
-#         self.node_year_dict = self.dataset[0]['node_year']
-#         self.edge_reltype_dict = self.dataset[0]['edge_reltype']
-#         self.y_dict = self.dataset[0]['y_dict']
-#         self.split_idx = torch.tensor(split_idx[split]['paper'])
-#         self._extract_split()
-
-#     def _extract_split(self):
-#         self.node_year_dict = {key: value[self.split_idx] for key, value in self.node_year_dict.items()}
-#         self.x_dict = {key: value[self.split_idx] for key, value in self.x_dict.items()}
-#         self.y_dict = {key: value[self.split_idx] for key, value in self.y_dict.items()}
-#         self.edge_index_dict = {key: value[:, self.split_idx] for key, value in self.edge_index_dict.items()}
-#         self.edge_reltype_dict = {key: value[self.split_idx] for key, value in self.edge_reltype_dict.items()}
-    
-#     def len(self):
-#         return len(self.split_idx)
-    
-#     def get(self, idx):
-#         return self.x_dict, self.edge_index_dict, self.edge_reltype_dict, self.y_dict, self.node_year_dict
-    
-
-# dataset = PygNodePropPredDataset(name = "ogbn-mag", root = 'dataset/')
-# graph=dataset[0]
-# graph.to('cpu')
-# import pdb;pdb.set_trace()
-# print(graph.edge_index.device)
-# train_idx = dataset.get_idx_split()["train"]
-# subgraph = torch_geometric.utils.subgraph(train_idx, graph)
-
-# print(dataset[0])
-    
-
 from ogb.nodeproppred import DglNodePropPredDataset
 import dgl
 import torch
@@ -64,23 +14,87 @@ train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_i
 # sg, inverse_indices = dgl.khop_subgraph(g, {'game': 0}, k=2)
 
 
-print(graph)
-
+k_hops=3
+paper_node_id=10
+author_node_id=100
 # Apply the mask to the original graph
+train_graph= dgl.remove_nodes(graph, valid_idx['paper'],ntype='paper')
+train_graph= dgl.remove_nodes(train_graph, train_graph.nodes('institution'),ntype='institution')
+train_graph= dgl.remove_nodes(train_graph, train_graph.nodes('field_of_study'),ntype='field_of_study')
 
-# train_graph= dgl.remove_nodes(graph, valid_idx['paper'],ntype='paper')
-# train_graph= dgl.remove_nodes(train_graph, test_idx['paper'],ntype='paper')
 # remove institute nodes
 # print(train_graph)
 # train_graph = dgl.remove_nodes(graph, graph.nodes('institution'),ntype='institution')
 # train_graph= dgl.remove_nodes(train_graph, graph
 
-k_hops=3
-paper_node_id=10
-author_node_id=100
+
 # out_sampled_graph = dgl.sampling.sample_neighbors(train_graph, {'paper':[paper_node_id]},k_hops,edge_dir ='out'  )
 # in_sampled_graph = dgl.sampling.sample_neighbors(train_graph, {'paper':[paper_node_id]},k_hops,edge_dir ='in' )
 # merged_graph=dgl.merge([out_sampled_graph, in_sampled_graph])
+
+def sample_author_pairs_full_random(graph,num_samples=100):
+    positive_pairs=torch.zeros(num_samples,2)
+    negative_pairs=torch.zeros(num_samples,2)
+    cfiller=0
+    anodes=graph.nodes(ntype='author')
+    while cfiller<(num_samples):
+        source=np.random.choice(anodes)
+        papers=graph.successors(source,etype='writes')
+        paper=np.random.choice(papers)
+        writers=graph.predecessors(paper,etype='writes')
+        for writer in writers:
+            if writer!=source:
+                positive_pairs[cfiller,0]=source
+                positive_pairs[cfiller,1]=writer
+                cfiller+=1
+                break
+
+    negative_pairs[:,0]=torch.tensor(np.random.choice(anodes,num_samples))
+    negative_pairs[:,1]=torch.tensor(np.random.choice(anodes,num_samples))
+
+    return positive_pairs,negative_pairs
+
+def sample_author_pairs(graph,num_samples=100):
+    positive_pairs=torch.zeros(num_samples,2)
+    negative_pairs=torch.zeros(num_samples,2)
+    cfiller=0
+    anodes=graph.nodes(ntype='author')
+    while cfiller<(num_samples):
+        source=np.random.choice(anodes)
+        papers=graph.successors(source,etype='writes')
+        for paper in papers:
+            if np.random.uniform()<0.8:
+                continue
+            writers=graph.predecessors(paper,etype='writes')
+            for writer in writers:
+                if np.random.uniform()<0.8:
+                    continue
+                if writer!=source:
+                    positive_pairs[cfiller,0]=source
+                    positive_pairs[cfiller,1]=writer
+                    cfiller+=1
+                    if cfiller==num_samples:
+                        break
+            if cfiller==num_samples:
+                break
+
+    negative_pairs[:,0]=torch.tensor(np.random.choice(anodes,num_samples))
+    negative_pairs[:,1]=torch.tensor(np.random.choice(anodes,num_samples))
+
+    # e1,e2=graph.edges(etype='writes')
+    # cnter=0
+    # for x in range(num_samples):
+    #     ps1=e2[e1==negative_pairs[x][0]]
+    #     ps2=e2[e1==negative_pairs[x][1]]
+    #     cp=np.intersect1d(ps1,ps2)
+    #     if len(cp)!=0:
+    #         cnter+=1
+    return positive_pairs,negative_pairs
+
+
+# sample_author_pairs_full_random(dgl.khop_subgraph(graph,{'paper':[paper_node_id]},4)[0])
+
+
 
 def sample_positive_negative_author(graph,author_node_id, k_hops=3, khop_weights = [0, 0.75, 0.25]):
     author_nodes = {}
@@ -100,6 +114,11 @@ def sample_positive_negative_author(graph,author_node_id, k_hops=3, khop_weights
     negatives = set(graph.nodes('author').tolist()).difference(sampled_graph.ndata[dgl.NID]['author'])
     negative = np.random.choice(list(negatives))
     return (author_node_id, positive.item()), (author_node_id, negative)
+
+
+
+
+
 
 print(sample_positive_negative_author(graph,author_node_id))
 
